0a0d0cb9482a3e630d56168fa4b9b49067cac285
AlekseyLavr <govola77@gmail.com>
2023-07-07 20:32:52 +0300

    Домашнее задание 2

---
+++ DZ2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+    Console.WriteLine($"max= {a}");
+else if (b == a)
+    Console.WriteLine("Числа равны");
+else
+    Console.WriteLine($"max= {b}");
