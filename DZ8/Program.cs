785c94de8ec1b8ee58c9f60d41d051026b93be99
AlekseyLavr <govola77@gmail.com>
2023-07-07 21:51:31 +0300

    Домашнее задание 8

---
+++ DZ8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i <= a)
+{
+    if (i % 2 == 0)
+        Console.Write(i +" ");
+    i++;
+}
