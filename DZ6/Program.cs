566edbacab1064d6b6b4f8e9431b13fc3185bf55
AlekseyLavr <govola77@gmail.com>
2023-07-07 21:01:32 +0300

    Домашнее задание 6

---
+++ DZ6/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N % 2 == 0)
+    Console.WriteLine("да");
+else
+    Console.WriteLine("нет");
