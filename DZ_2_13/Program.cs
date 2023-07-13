b48aa63f21a4a0f926af505df46a328c98880494
AlekseyLavr <govola77@gmail.com>
2023-07-13 19:59:53 +0300

    Домашнее задание № 13

---
+++ DZ_2_13/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int ThirdDigit(int number)
+{
+    int result = -1;
+    if (number >= 100)
+    {
+        while (number > 999)
+        {
+            number = number / 10;
+        }
+        result = number % 10;
+    }
+    return result; 
+}
+if (number < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else Console.WriteLine(ThirdDigit(number));
