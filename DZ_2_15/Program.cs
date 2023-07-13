b919a1d31a3ed97c66fbef69d71f26f912d4c8c4
AlekseyLavr <govola77@gmail.com>
2023-07-13 20:45:30 +0300

    Домашнее задание № 13

---
+++ DZ_2_15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите номер дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 6 || number == 7)
+    Console.WriteLine("Ура, выходной!");
+else if (number == 1 || number == 2 || number == 3 || number == 4 || number == 5)
+{
+    Console.WriteLine("На работу.");
+}
+else
+{
+    Console.WriteLine("Такого дня не существует");
+}
